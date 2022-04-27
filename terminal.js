72ef0fc5f9ebe08b8960ce0db7989f03b0ccd208
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-04-27 19:13:03 +0200

    Add Terminal notes

---
+++ terminal.js
@@ -0,0 +1,15 @@
+/* 
+ls - list the contents of our current directory
+ls -a - list all files, including hidden files
+start . - open the curent directory in windows explorer
+ls folderName - list the contents of folder of our choice
+pwd - show us current location
+cd - change directory
+cd .. - go back one directory
+touch filename.js file2.txt - make new file with or make multiple files
+mkdir - create folder or folders
+____________
+rm - remove a file PERMANENTLY, it doesn't go into Recycle Bin!
+rm -rf - remove a folder PERMANENTLY, -rf is a flag that allows us to remove a folder
+____________
+*/
